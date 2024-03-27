55d2e56ce3b9cc59181291eab37d0c3b6979716b
gaeull <moonga10@naver.com>
2024-03-27 18:04:32 +0900

    [fix] user, video controller 예외처리

--- factoryvision/src/main/java/webproject/factoryvision/domain/user/controller/UserController.java
+++ factoryvision/src/main/java/webproject/factoryvision/domain/user/controller/UserController.java
@@ -6,13 +6,12 @@ import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import webproject.factoryvision.domain.user.dto.*;
-//import webproject.factoryvision.domain.user.service.TokenProvider;
-//import webproject.factoryvision.domain.user.service.TokenService;
 import webproject.factoryvision.domain.user.entity.User;
 import webproject.factoryvision.domain.user.mapper.UserMapper;
 import webproject.factoryvision.domain.user.repository.UserRepository;
@@ -35,39 +34,54 @@ public class UserController {
     private final TokenProvider tokenProvider;
     private final UserMapper userMapper;
     private final UserRepository userRepository;
+    private final HttpHeaders getHeaders;
 
     // 전체 사용자 정보 조회
 //    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("/userInfo")
     @Operation(summary = "전체 사용자 정보 조회")
     public ResponseEntity<List<UserDto>> getAllUsers() {
-        return userService.getAllUsers();
+        List<UserDto> allUsers = userService.getAllUsers();
+        return ResponseEntity.status(HttpStatus.OK).body(allUsers);
     }
 
     // 사용자 id별 정보 조회
     @GetMapping("/userInfo/{id}")
     @Operation(summary = "특정 사용자 정보 조회(id로 구분)")
-    public ResponseEntity<GetUserInfoResponse> getUserInfoByUserId(@PathVariable Long id) {
-        return ResponseEntity.ok(userService.getUserById(id));
+    public ResponseEntity<?> getUserInfoByUserId(@PathVariable Long id) {
+        GetUserInfoResponse user = userService.getUserById(id);
+        return user != null ? ResponseEntity.status(HttpStatus.OK).body(user) : ResponseEntity.status(HttpStatus.NOT_FOUND).body("해당되는 사용자가 없습니다.");
     }
 
     // 회원가입
     @PostMapping("/signup")
     @Operation(summary = "회원가입", description = "request body에 회원정보 입력, user로 회원가입할경우 role:USER로, 정보 입력할때 값에 공백안들어가게 주의하기!!")
-    public ResponseEntity<Void> signup(@RequestBody SignUpRequest request) {
-        SignUpResponse response = userService.signUp(request);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    public ResponseEntity<?> signup(@RequestBody SignUpRequest request) {
+        userService.signUp(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .headers(getHeaders)
+                .body("회원가입 완료");
     }
 
     // 로그인, accesstoken, refreshtoken이 생성해서 헤더로 제공.
     // refreshtoken은 레디스에 저장. accesstoken 만료되면, refreshtoken이용해서 accesstoken재발급에 사용됨.
     @PostMapping("/login")
     @Operation(summary = "로그인", description = "로그인id, 비밀번호 입력, 로그인 성공시 생성되는 accessToken 헤더에 포함시켜서 다른 api 요청하기")
-    public ResponseEntity<TokenResponse> login(@RequestBody SignInRequest request, HttpServletResponse response) {
-        SignInResponse user = userService.login(request);
-        TokenResponse token = tokenProvider.createTokenByLogin(user.getUserId(), user.getRole());//atk, rtk 생성
-        response.addHeader(tokenProvider.AUTHORIZATION_HEADER, token.getAccessToken());// 헤더에 에세스 토큰만 싣기
-        return ResponseEntity.ok(token);
+    public ResponseEntity<?> login(@RequestBody SignInRequest request, HttpServletResponse response) {
+        try {
+            SignInResponse user = userService.login(request);
+            TokenResponse token = tokenProvider.createTokenByLogin(user.getUserId(), user.getRole());//atk, rtk 생성
+            response.addHeader(tokenProvider.AUTHORIZATION_HEADER, token.getAccessToken());// 헤더에 에세스 토큰만 싣기
+            return ResponseEntity.status(HttpStatus.OK).body(token);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .headers(getHeaders)
+                    .body("비밀번호가 일치하지 않습니다.");
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .headers(getHeaders)
+                    .body("사용자 정보가 없습니다.");
+        }
     }
 
     // 로그아웃
@@ -75,9 +89,12 @@ public class UserController {
     // 로그아웃 --> 레디스에 저장된 refreshToken 삭제
     @DeleteMapping("/logout")
     @Operation(summary = "로그아웃")
-    public ResponseEntity logout(@AuthenticationPrincipal UserDetailsImpl userDetails, HttpServletRequest request){
+    public ResponseEntity<?> logout(@AuthenticationPrincipal UserDetailsImpl userDetails, HttpServletRequest request){
         String accessToken = tokenProvider.resolveToken(request);
-        return userService.logout(accessToken,userDetails.getUserId());
+        userService.logout(accessToken, userDetails.getUserId());
+        return ResponseEntity.status(HttpStatus.OK)
+                .headers(getHeaders)
+                .body("로그아웃 완료");
     }
 
     // AccessToken  재발급
@@ -85,27 +102,44 @@ public class UserController {
     // 그럼 클라이언트에서 에러메세지를 확인 후, 이 api(atk 재발급)를 요청함.
     @PostMapping("/reissue-token")
     @Operation(summary = "accessToken 재발급", description = "API호출할때마다 시큐리티필터를 통해 인증 인가를 받음. 이때 만료된 토큰인지 검증하고 만료시 만료된토큰임을 에러메세지로 보냄, 그럼 클라이언트에서 에러메세지를 확인 후, 이 api(atk 재발급)를 요청함. 새로 생성한 accesstoken사용하기")
-    public TokenResponse reissueToken(@AuthenticationPrincipal UserDetailsImpl userDetails,
+    public ResponseEntity<?> reissueToken(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                       @RequestBody ReissueTokenRequest tokenRequest){
-        //user 정보를 이용하여 토큰 발행
-        SignUpResponse user = userMapper.toDto(userDetails.getUser());
-        return tokenProvider.reissueAtk(user.getUserId(), user.getRole(), tokenRequest.getRefreshToken());
+        try {
+            //user 정보를 이용하여 토큰 발행
+            SignUpResponse user = userMapper.toDto(userDetails.getUser());
+            TokenResponse tokenResponse = tokenProvider.reissueAtk(user.getUserId(), user.getRole(), tokenRequest.getRefreshToken());
+            return ResponseEntity.status(HttpStatus.OK).body(tokenResponse);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .headers(getHeaders)
+                    .body("유효하지 않은 refreshToken 입니다.");
+        }
     }
 
     // 사용자 정보 수정
     @PostMapping("/{id}")
     @Operation(summary = "사용자 정보 수정(id로)", description = "id넣고 request body에 수정사항 반영하기")
-    public ResponseEntity<Void> updateUser(@PathVariable("id") long id, @RequestBody UpdateUserDto request) {
-        userService.updateUser(id, request);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<?> updateUser(@PathVariable("id") long id, @RequestBody UpdateUserDto request) {
+        try {
+            userService.updateUser(id, request);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .headers(getHeaders)
+                    .body("사용자 정보 수정 완료");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .headers(getHeaders)
+                    .body("해당 사용자 정보 없음");
+        }
     }
 
     // 회원 탈퇴
     @DeleteMapping("/{id}")
     @Operation(summary = "회원 탈퇴(id로)", description = "id넣으면 사용자 정보 지워짐")
-    public ResponseEntity<Void> deleteUser(@PathVariable("id") long id) {
+    public ResponseEntity<?> deleteUser(@PathVariable("id") long id) {
         userService.deleteUser(id);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.OK)
+                .headers(getHeaders)
+                .body("회원 탈퇴 완료");
     }
 
     // 토큰에서 유저 정보 가져오기
--- factoryvision/src/main/java/webproject/factoryvision/domain/user/service/UserService.java
+++ factoryvision/src/main/java/webproject/factoryvision/domain/user/service/UserService.java
@@ -1,5 +1,6 @@
 package webproject.factoryvision.domain.user.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -33,19 +34,18 @@ public class UserService {
     private final RedisDao redisDao;
     private final SecurityConfig securityConfig;
 
-    public Optional<User> getUserByUserId(String userId) {
-        return userRepository.findByUserId(userId);
-    }
+//    public Optional<User> getUserByUserId(String userId) {
+//        return userRepository.findByUserId(userId);
+//    }
 
     // 사용자 id별 정보 조회
     public GetUserInfoResponse getUserById(Long id) {
         return userMapper.toUserInfo(userRepository.findById(id).orElse(null));
     }
 
-    public ResponseEntity<List<UserDto>> getAllUsers() {
+    public List<UserDto> getAllUsers() {
         List<User> userList = userRepository.findAll();
-        List<UserDto> userDtolist = userMapper.toUserDtolist(userList);
-        return ResponseEntity.ok(userDtolist);
+        return userMapper.toUserDtolist(userList);
     }
 
     @Transactional
@@ -73,7 +73,7 @@ public class UserService {
     @Cacheable(cacheNames = CacheNames.LOGINUSER, key = "'login'+ #p0.getUserId()", unless = "#result == null")
     @Transactional
     public SignInResponse login(SignInRequest request) {
-        User user = userRepository.findByUserId(request.getUserId()).orElseThrow(() -> new IllegalArgumentException("사용자 정보가 없습니다."));
+        User user = userRepository.findByUserId(request.getUserId()).orElseThrow(() -> new EntityNotFoundException("사용자 정보가 없습니다."));
         if (!securityConfig.PasswordEncoder().matches(request.getPassword(), user.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
@@ -82,7 +82,7 @@ public class UserService {
 
     @CacheEvict(cacheNames = CacheNames.USERBYUSERID, key = "'login'+#p1")
     @Transactional
-    public ResponseEntity logout(String accessToken, String userId) {
+    public void logout(String accessToken, String userId) {
         // 레디스에 accessToken 사용못하도록 등록
         Long expiration = tokenProvider.getExpiration(accessToken);
 
@@ -97,7 +97,6 @@ public class UserService {
         } else {
             throw new IllegalArgumentException("이미 로그아웃한 유저입니다.");
         }
-        return ResponseEntity.ok("로그아웃 완료");
     }
 
     // 로그인 - 필터 사용
